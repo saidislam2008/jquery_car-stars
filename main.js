3dccfd4748862f66cf2c242ab25e89817d6b12f7
saidislam2008 <saidislammamasaliev@gmail.com>
2023-02-08 15:59:07 +0600

    eleventh commit

---
+++ main.js
@@ -0,0 +1,59 @@
+// let img1 = document.querySelector('.img1')
+// let img2 = document.querySelector('.img2')
+// let img3 = document.querySelector('.img3')
+// let img4 = document.querySelector('.img4')
+// let img5 = document.querySelector('.img5')
+// let img6 = document.querySelector('.img6')
+
+// function allFunc(){
+//     function f1(){
+//     img1.style.opacity = '1'
+// } 
+// setTimeout(f1 , 1500)
+
+// function f2(){
+//     img2.style.opacity = '1'
+// } 
+// setTimeout(f2 , 3500)
+
+// function f3(){
+//     img3.style.opacity = '1'
+//     img1.style.opacity = '0'
+// } 
+// setTimeout(f3 , 5000)
+
+// function f4(){
+//     img4.style.opacity = '1'
+//     img2.style.opacity = '0'
+// } 
+// setTimeout(f4 , 6500)
+
+// function f5(){
+//     img5.style.opacity = '1'
+//     img4.style.opacity = '0'
+// } 
+// setTimeout(f5 , 8000)
+
+// function f6(){
+//     img6.style.opacity = '1'
+//     img3.style.opacity = '0'
+// } 
+
+// function extra(){
+//     img6.style.opacity = '0'
+//     img3.style.opacity = '0'
+// }
+// setTimeout(extra , 9000)
+
+// function extra2(){
+//     img5.style.opacity = '0'
+// }
+// setTimeout(extra2 , 10500)
+// }
+// allFunc()
+// setInterval(allFunc , 10500)
+
+function driving(){
+    $('.car').css({ "width" : "50px" , "top" : "550px" })
+}
+driving()
